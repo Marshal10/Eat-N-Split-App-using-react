e441ba9bea11c1122ce698c930b52d199b493846
Marshal10 <marshalmaleane123@gmail.com>
2024-06-01 14:34:27 +0530

    Button and initial stylings

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ const initialFriends = [
 
 export default function App() {
   return (
-    <div className="App">
+    <div className="app">
       <div className="left">
         <FriendsList />
         <FriendAddForm />
